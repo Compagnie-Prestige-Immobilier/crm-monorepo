79560075895b916345a88f29e1cfee23f4cb6b7a
traorecheikh <atidiane741@gmail.com>
2026-09-07 00:11:52 +0000

    feat: implement redis

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,10 @@ import 'data/local/database.dart';
 import 'data/repositories/draft_repository.dart';
 
 Future<void> main() async {
+  void mark(String s) => Timeline.instantSync('CPIPERF-$s');
+  mark('enter');
   WidgetsFlutterBinding.ensureInitialized();
+  mark('binding');
 
   if (kDebugMode) {
     SemanticsBinding.instance.ensureSemantics();
@@ -26,6 +30,7 @@ Future<void> main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  mark('orientation');
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -34,12 +39,16 @@ Future<void> main() async {
   );
 
   final AppDatabase database = openAppDatabase();
+  mark('db');
   final SharedPreferences preferences = await SharedPreferences.getInstance();
+  mark('prefs');
   final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  mark('packageinfo');
 
   unawaited(DraftRepository(database).purgeStale());
 
   unawaited(_initBackground());
+  mark('runApp');
 
   runApp(
     ProviderScope(
