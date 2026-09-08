34eb8c528ded2b4aa2100d3626affd33d4b0a176
traorecheikh <atidiane741@gmail.com>
2026-09-08 17:17:09 +0000

    feat: fiches

--- apps/mobile/lib/core/utils/ids.dart
+++
@@ -1,9 +0,0 @@
-import 'package:uuid/uuid.dart';
-
-abstract final class Ids {
-  static const Uuid _uuid = Uuid();
-
-  static String newId() => _uuid.v7();
-
-  static String newRequestId() => _uuid.v7();
-}
--- apps/mobile/lib/core/utils/relative_time.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-String relativeTime(DateTime when, {DateTime? now}) {
-  final Duration delta = (now ?? DateTime.now()).difference(when);
-  if (delta.isNegative || delta.inMinutes < 1) return 'à l\'instant';
-  if (delta.inHours < 1) return 'il y a ${delta.inMinutes} min';
-  if (delta.inDays < 1) return 'il y a ${delta.inHours} h';
-  return 'le ${DateFormat('d MMMM à HH:mm', 'fr').format(when)}';
-}
