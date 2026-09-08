682173875d1b04c760f8cea82122019ab48c9194
traorecheikh <atidiane741@gmail.com>
2026-09-08 17:17:09 +0000

    feat: fiches

--- apps/mobile/lib/core/network/session_expired.dart
+++
@@ -1,8 +0,0 @@
-class SessionExpired implements Exception {
-  const SessionExpired([this.cause]);
-
-  final Object? cause;
-
-  @override
-  String toString() => 'SessionExpired(${cause ?? 'refresh refusé'})';
-}
